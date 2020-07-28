80fc7637a166886a440f3bffe47ecf49c768561f
Gabriele Sarti <gabriele.sarti996@gmail.com>
2020-07-28 17:15:37 +0200

    Updated with new confusion matrices

--- scripts/plot_conf_matrices.R
+++ scripts/plot_conf_matrices.R
@@ -32,7 +32,7 @@ plot_cm <- function(fname, tit) {
       plot.title = element_text()
     )
 }
-
+  
 svevo_ctx <- plot_cm('generated_text_labels=contextual_samples=3_predicted.csv', tit="Svevo CTX")
 svevo_ctx_ht <- plot_cm('generated_text_labels=contextual_samples=3_high_temp_predicted.csv', tit="Svevo CTX High Temp.")
 eur_it <- plot_cm('generated_text_labels_europarl_it=contextual_samples=3_predicted_restricted.csv', tit="Europarl-IT CTX")
@@ -42,3 +42,35 @@ eur_en_ht <- plot_cm('generated_text_labels_europarl_en=contextual_samples=3_hig
 grid.arrange(svevo_ctx, svevo_ctx_ht, nrow=1)
 grid.arrange(eur_it, eur_it_ht, nrow=1)
 grid.arrange(eur_en, eur_en_ht, nrow=1)
+
+svevo_tuned <- plot_cm('svevo_generated_tuned_predicted.csv', tit="Svevo Tuned")
+grid.arrange(svevo_ctx, svevo_tuned, nrow=1)
+eur_en_tuned <- plot_cm('generated_EuroParlEng_contextual_samp=1_iters=20_temp=1_0_gm=0_95.csv', tit="Europarl-EN CTX Tuned")
+grid.arrange(eur_en, eur_en_tuned, nrow=1)
+eur_it_tuned <- plot_cm('generated_EuroParlIta_contextual_samp=1_iters=20_temp=1_0_gm=0_95.csv', tit="Europarl-IT CTX Tuned")
+grid.arrange(eur_it, eur_it_tuned, nrow=1)
+
+
+  # Plot topic modeling results
+
+data <- read.csv('topic_modeling_results.csv') %>% filter(dataset %in% c("Svevo Corpus"))
+data$dataset <- factor(data$dataset, levels=c("EuroParl EN", "EuroParl IT"))
+data$type <- factor(data$type, levels=c("combined", "contextual"))
+number_ticks <- function(n) {function(limits) pretty(limits, n)}
+palette = c("#D85885", "#7694C1")
+ggplot(data, aes(x=type, y=npmi, fill=type)) + 
+  geom_col() +
+  facet_grid(~ n_topics, scales="free_y") +
+  scale_y_continuous(breaks=scales::pretty_breaks(3)) +
+  scale_fill_manual(values = palette) + 
+  ylab("NPMI") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.y = element_text(vjust=2),
+        legend.direction = "horizontal",
+        legend.title = element_blank(),
+        legend.position = "bottom",
+        legend.text = element_text(margin = margin(r = 10, unit = "pt")),
+        strip.text.y = element_text(face="bold"))
+ggsave("sentence_scores.png", units="mm", width=183, height=118, dpi=300)
